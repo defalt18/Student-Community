3e9d4b7fd49ae05e476fb741880f66fd518188dd
Kushagra Pathak <201801005@daiict.ac.in>
2021-10-19 11:16:48 +0530

    route resolved

--- src/components/NewHeader/index.js
+++ src/components/NewHeader/index.js
@@ -8,6 +8,7 @@ import SearchPlayer from './components/SearchPlayer'
 import { Link, useHistory } from 'react-router-dom'
 import { useAuthListener } from 'hooks'
 import { useFirestore, useFirestoreDocData } from 'reactfire'
+import { HOME } from 'constants/routes'
 
 function NewHeader() {
 	const { user } = useAuthListener()
@@ -20,9 +21,10 @@ function NewHeader() {
 		() => history.push(`/${user?.uid}/new-profile`),
 		[user.uid, history]
 	)
+
 	return (
 		<div className='z-20 fixed top-0 border-b border-header_border_blue bg-component_core w-screen flex items-center h-16 px-3'>
-			<Link to='/' className='w-40 flex text-decoration-none gap-x-2'>
+			<Link to={HOME} className='w-40 flex text-decoration-none gap-x-2'>
 				<MediaContainer src={da_logo} className='h-10 w-auto object-contain' />
 				<p className='text-secondary text-white'>Student Community</p>
 			</Link>
@@ -31,7 +33,7 @@ function NewHeader() {
 					<>
 						<SearchPlayer />
 						<Button className='rounded-3xl p-2'>
-							<Notifications color={'inherit'} />
+							<Notifications color='inherit' />
 						</Button>
 						<Button callback={redirectToProfile} className='rounded-3xl p-2'>
 							<Avatar src={userdata?.image} size='small' variant='normal' />
@@ -40,7 +42,7 @@ function NewHeader() {
 							text='Create post'
 							variant='filled'
 							size='medium'
-							className='px-12 py-2'
+							className='w-48 h-10 flex items-center justify-center'
 						/>
 					</>
 				) : (
